b3255a38e84e179c750c9b0be9e8d4ea434a5664
Maanil Verma <vermaMaanil97@gmail.com>
2024-02-12 09:54:01 +0530

    chore: add dummy data for service tiles

---
+++ utils/data.ts
@@ -0,0 +1,37 @@
+export const serviceTilesData = [
+  {
+    id: "1",
+    text: "Project Management Tool Design",
+    ovalBackgroundColor: "linear-gradient(to right, #645D6F, #27232E)",
+    borderColor: "#9B44D5",
+    circleBackground: "#773EA5",
+  },
+  {
+    id: "2",
+    text: "The Leading Customer Data Platform",
+    ovalBackgroundColor: "linear-gradient(to right, #2b2a27, #53524e)",
+    borderColor: "#756e52",
+    circleBackground: "#756e52",
+  },
+  {
+    id: "3",
+    text: "Food Delivery Mobile App",
+    ovalBackgroundColor: "linear-gradient(to top, #2B2728, #4C3840)",
+    borderColor: "#C188AC",
+    circleBackground: "#986178",
+  },
+  {
+    id: "4",
+    text: "Business Management Dashboard",
+    ovalBackgroundColor: "linear-gradient(to bottom, #2B496F, #33383D)",
+    borderColor: "#2B8AF8",
+    circleBackground: "#2B8AF8",
+  },
+  {
+    id: "5",
+    text: "Project Management Tool Design",
+    ovalBackgroundColor: "linear-gradient(to right, #645D6F, #27232E)",
+    borderColor: "#9B44D5",
+    circleBackground: "#773EA5",
+  },
+];
